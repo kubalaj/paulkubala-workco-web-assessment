6603ffc43a2c3260329a34f29c83575f436e8782
kubalaj <jp@jpkubala.com>
2019-03-25 13:04:03 -0700

    adding sass and some boilder styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import ProductsContainer from './ProductsContainer'
-import CartContainer from './CartContainer'
+import React from "react";
+import ProductsContainer from "./ProductsContainer";
+import CartContainer from "./CartContainer";
 
+require("../styles/main.scss");
 const App = () => (
   <div>
     <h2>Shopping Cart Example</h2>
-    <hr/>
+    <hr />
     <ProductsContainer />
-    <hr/>
+    <hr />
     <CartContainer />
   </div>
-)
+);
 
-export default App
+export default App;
